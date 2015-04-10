5ce0a7fdc8f4ff75d580d55ebdc8b194fb8542f4
Sebastian Wiesner <swiesner@lunaryorn.com>
2015-04-10 08:55:30 +0200

    Improve Scala indentation setup

--- init.el
+++ init.el
@@ -1370,7 +1370,9 @@ Disable the highlighting of overlong lines."
 
 (use-package scala-mode2                ; Scala editing
   :ensure t
-  :defer t)
+  :defer t
+  :config '(setq scala-indent:default-run-on-strategy
+                 scala-indent:operator-strategy))
 
 (use-package flycheck-auto-scalastyle   ; Scalastyle setup
   :load-path "lisp/"
