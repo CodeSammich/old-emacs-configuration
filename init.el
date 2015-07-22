27b10425e4d84bf2216e0823c4ea423746e8ea86
Sebastian Wiesner <swiesner@lunaryorn.com>
2015-07-22 09:29:05 +0200

    Remove redundant character from test range

--- init.el
+++ init.el
@@ -200,7 +200,7 @@
 (set-frame-font "Source Code Pro-13" nil t)   ; Default font
 
 ;; Additional fonts for special characters and fallbacks
-;; Test range: 🐷 ⊄ ∫ 𝛼 α 🜚 𝆹𝅥𝅯
+;; Test range: 🐷 ⊄ ∫ 𝛼 α 🜚
 (when (eq system-type 'darwin)
   ;; Colored Emoji on OS X
   (set-fontset-font t 'symbol (font-spec :family "Apple Color Emoji")
