8b7e8a2969f413ef93bd6583f46dc7f189e6de63
Sebastian Wiesner <swiesner@lunaryorn.com>
2015-08-07 11:49:30 +0200

    Add command to count lines of my Emacs config

--- lisp/lunaryorn-files.el
+++ lisp/lunaryorn-files.el
@@ -143,6 +143,40 @@ Otherwise copy the non-directory part only."
   (interactive)
   (byte-recompile-directory package-user-dir nil 'force))
 
+(defun lunaryorn-all-init-files (&optional with-packages)
+  "Return a list of all Emacs Lisp files in my configuration.
+
+If WITH-PACKAGES is given and non-nil include 3rd party
+packages."
+  (append (list user-init-file)
+          (directory-files-recursively (locate-user-emacs-file "lisp/")
+                                       (rx ".el" eos))
+          (if with-packages
+              (directory-files-recursively package-user-dir
+                                           (rx ".el" eos))
+            nil)))
+
+;;;###autoload
+(defun lunaryorn-count-config-lines (&optional with-packages)
+  "Show a buffer with LoC statistics for my Emacs config.
+
+If WITH-PACKAGES is given and non-nil include 3rd party packages
+into the count."
+  (interactive "P")
+  (let ((cloc (executable-find "cloc")))
+    (unless cloc
+      (user-error "Please install cloc"))
+    (with-current-buffer (get-buffer-create " *LoC Emacs configuration*")
+      (text-mode)
+      (read-only-mode)
+      (view-mode)
+      (let ((inhibit-read-only t)
+            (files (lunaryorn-all-init-files with-packages)))
+        (erase-buffer)
+        (goto-char (point-min))
+        (apply #'call-process cloc nil t t "--quiet" files))
+      (pop-to-buffer (current-buffer)))))
+
 
 ;;; IntelliJ integration
 (defun lunaryorn-intellij-project-root-p (directory)
