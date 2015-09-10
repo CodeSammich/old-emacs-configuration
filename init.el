165dcbdead07cf51a61085d0e7744c545f7a3fac
Sebastian Wiesner <swiesner@lunaryorn.com>
2015-09-10 10:43:59 +0200

    Add toggle bindings for goto address modes

--- init.el
+++ init.el
@@ -2347,6 +2347,8 @@ Install mudraw with brew install mupdf-tools"))))
 
 (use-package goto-addr                  ; Make links clickable
   :defer t
+  :bind (("C-c t a" . goto-address-mode)
+         ("C-c t A" . goto-address-prog-mode))
   :init (progn (add-hook 'prog-mode-hook #'goto-address-prog-mode)
                (add-hook 'text-mode-hook #'goto-address-mode)))
 
