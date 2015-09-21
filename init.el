4d9b18014b5694c3e6eecac5438e21b6c7f5a05a
Sebastian Wiesner <swiesner@lunaryorn.com>
2015-09-21 09:16:24 +0200

    Configure gh.el

--- init.el
+++ init.el
@@ -2179,6 +2179,12 @@ Disable the highlighting of overlong lines."
 
 
 ;;; Github integration
+(use-package gh                         ; Github API library
+  ;; Don't ensure it, since it's only brought in as dependency
+  :defer t
+  ;; Change the default profile.  The profile itself is set up via customize,
+  ;; and includes auth data, to prevent it from storing tokens in Git config
+  :config (setq gh-profile-default-profile "lunaryorn"))
 
 (use-package magit-gh-pulls             ; Show Github PRs in Magit
   :ensure t
