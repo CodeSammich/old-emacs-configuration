3765d033153ef1690520c987a87e44ff138f1441
Sebastian Wiesner <swiesner@lunaryorn.com>
2016-05-19 22:04:14 +0200

    Add preliminary Haskell setup

--- init.el
+++ init.el
@@ -391,6 +391,10 @@ symbols, emojis, greek letters, as well as fall backs for."
     "C-c m" "scala/personal"
     "C-c m b" "scala/build")
 
+  (which-key-declare-prefixes-for-mode 'haskell-mode
+    "C-c m" "haskell/personal"
+    "C-c m i" "haskell/imports")
+
   (which-key-declare-prefixes-for-mode 'rust-mode
     "C-c C-c" "rust/cargo")
 
@@ -1914,6 +1918,12 @@ Taken from http://stackoverflow.com/a/3072831/355252."
   (setq-default eldoc-documentation-function #'describe-char-eldoc)
   :diminish (eldoc-mode . " ⓓ"))
 
+(use-package etags                      ; Tag navigation
+  :defer t
+  :config
+  ;; Do not query before reverting TAGS tables
+  (setq tags-revert-without-query t))
+
 (use-package restclient                 ; ReST REPL for Emacs
   :ensure t
   :defer t)
@@ -2090,6 +2100,66 @@ the REPL in a new frame instead."
   :disabled t
   :load-path "lisp/"
   :defer t)
+
+
+;;; Haskell
+(use-package haskell-mode               ; Haskell major mode
+  :ensure t
+  :defer t
+  :bind (:map haskell-mode-map
+              ("M-." . haskell-mode-jump-to-def-or-tag)
+              ("C-c m i j" . haskell-navigate-imports)
+              ("C-c m i s" . haskell-sort-imports)
+              ("C-c m i a" . haskell-align-imports)
+              ;; Recommended Haskell Mode bindings, see
+              ;; http://haskell.github.io/haskell-mode/manual/latest/Interactive-Haskell.html
+            )
+  :config
+  (setq haskell-tags-on-save t          ; Regenerate TAGS on save
+        haskell-process-log t           ; Show log for GHCI process
+        ;; Remove unused imports and auto-import modules
+        haskell-process-suggest-remove-import-lines t
+        haskell-process-auto-import-loaded-modules t)
+
+  (add-hook 'haskell-mode-hook #'haskell-decl-scan-mode) ; IMenu support
+  (add-hook 'haskell-mode-hook #'interactive-haskell-mode))
+
+(use-package haskell                    ; Interactive Haskell
+  :ensure haskell-mode
+  :defer t
+  :after haskell-mode
+  :bind (:map haskell-mode-map
+         ("C-c C-l" . haskell-process-load-file)
+         ("C-`" . haskell-interactive-bring)
+         ("C-c C-t" . haskell-process-do-type)
+         ("C-c C-i" . haskell-process-do-info)
+         ("C-c C-c" . haskell-process-cabal-build)
+         ("C-c C-k" . haskell-interactive-mode-clear)
+         ("C-c c" . haskell-process-cabal)
+         :map interactive-haskell-mode-map
+         ("C-c m t" . haskell-mode-show-type-at))
+  :init (add-hook 'haskell-mode-hook 'interactive-haskell-mode))
+
+(use-package haskell-compile            ; Haskell compilation
+  :ensure haskell-mode
+  :defer t
+  ;; :after haskell-mode
+  :bind (:map haskell-mode-map
+              ("C-c m c" . haskell-compile)
+              ("<f5>" . haskell-compile))
+  :config
+  ;; Build with Stack
+  (setq haskell-compile-cabal-build-command "stack build"))
+
+(use-package cabal-mode                 ; Cabal files
+  :ensure haskell-mode
+  :defer t
+  :bind (:map haskell-cabal-mode-map
+              ("C-`" . haskell-interactive-bring)
+              ("C-c C-k" . haskell-interactive-mode-clear)
+              ("C-c C-c" . haskell-process-cabal-build)
+              ("C-c c" . haskell-process-cabal)))
+
 
 
 ;;; Python
