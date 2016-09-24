815b50e59e61c6942d95575b4195924d1ed35121
Sebastian Wiesner <swiesner@lunaryorn.com>
2016-09-24 11:19:28 +0200

    Try and pin Flycheck to MELPA

--- init.el
+++ init.el
@@ -45,14 +45,18 @@
       ;; Package archives, the usual suspects
       '(("GNU ELPA"     . "http://elpa.gnu.org/packages/")
         ("MELPA Stable" . "https://stable.melpa.org/packages/")
-        ("MELPA"        . "https://melpa.org/packages/")))
-(setq package-archive-priorities
+        ("MELPA"        . "https://melpa.org/packages/"))
       ;; Prefer MELPA Stable over GNU over MELPA.  IOW prefer MELPA's stable
       ;; packages over everything and only fall back to GNU or MELPA if
       ;; necessary.
+      package-archive-priorities
       '(("MELPA Stable" . 10)
         ("GNU ELPA"     . 5)
-        ("MELPA"        . 0)))
+        ("MELPA"        . 0))
+      ;; Pin a couple of my packages to MELPA; I maintain these and know what
+      ;; changes
+      package-pinned-packages
+      '(("flycheck" . "MELPA")))
 
 (package-initialize)
 
