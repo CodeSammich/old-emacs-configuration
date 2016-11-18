809d4b5cbbd05bf1ec644e0b91c3b13f227865e8
Sebastian Wiesner <swiesner@lunaryorn.com>
2016-11-18 14:38:11 +0100

    Remove redundant font setup
    Emacs 25.1 has made major improvements here

--- init.el
+++ init.el
@@ -242,40 +242,6 @@ the command line runner."
 (set-face-attribute 'variable-pitch nil
                     :family "Fira Sans" :height 130 :weight 'regular)
 
-;; Font setup
-(defun lunaryorn-configure-fonts (frame)
-  "Set up fonts for FRAME.
-
-Set the default font, and configure various overrides for
-symbols, emojis, greek letters, as well as fall backs for."
-  ;; Additional fonts for special characters and fallbacks
-  ;; Test range: 🐷 ❤ ⊄ ∫ 𝛼 α 🜚 Ⓚ
-  (dolist (script '(symbol mathematical))
-    (set-fontset-font t script (font-spec :family "XITS Math")
-                      frame 'prepend))
-
-  ;; Define a font set stack for symbols, greek and math characters
-  (dolist (script '(symbol greek mathematical))
-    (set-fontset-font t script (font-spec :family "Arial Unicode MS")
-                      frame 'prepend)
-    (set-fontset-font t script (font-spec :family "Menlo")
-                      frame 'prepend)
-    (set-fontset-font t script (font-spec :family "DejaVu Sans Mono")
-                      frame 'prepend))
-
-  (when (eq system-type 'darwin)
-    ;; Colored Emoji on OS X, prefer over everything else!
-    (set-fontset-font t nil (font-spec :family "Apple Color Emoji")
-                      frame 'prepend))
-
-  ;; Fallbacks for math and generic symbols
-  (set-fontset-font t nil (font-spec :family "Apple Symbols")
-                    frame 'append))
-
-(when-let (frame (selected-frame))
-  (lunaryorn-configure-fonts frame))
-(add-hook 'after-make-frame-functions #'lunaryorn-configure-fonts)
-
 (use-package face-remap                 ; Face remapping
   :bind (("C-c w z" . text-scale-adjust)))
 
